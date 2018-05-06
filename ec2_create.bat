c28ca34d80cbd3a04100356d69a02350641e4f9b
onishtar <onishtar@hotmail.com>
2018-05-06 16:12:27 -0400

    corrected security group on ec2

--- ec2_create.bat
+++ ec2_create.bat
@@ -1,2 +1,2 @@
-aws ec2 run-instances --image-id ami-43a15f3e --count 1 --instance-type t2.micro --key-name Essentials_Key --security-group-ids sg-3dfcd04b --subnet-id subnet-cbee92af --user-data file://ec2_userdata.txt
+aws ec2 run-instances --image-id ami-43a15f3e --count 1 --instance-type t2.micro --key-name Essentials_Key --security-group-ids sg-c010f988 --subnet-id subnet-cbee92af --user-data file://ec2_userdata.txt
 
