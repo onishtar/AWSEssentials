f680a1c2456c97f19d29f1fcadf227554c3e3cfb
onishtar <onishtar@hotmail.com>
2018-05-07 13:59:47 +0000

    removed count

--- ec2_create.sh
+++ ec2_create.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws ec2 run-instances --image-id ami-43a15f3e --count 1 --instance-type t2.micro --key-name Essentials_Key --security-group-ids sg-c010f988 --subnet-id subnet-cbee92af --user-data file://ec2_userdata.txt
+aws ec2 run-instances --image-id ami-43a15f3e --instance-type t2.micro --key-name Essentials_Key --security-group-ids sg-c010f988 --subnet-id subnet-cbee92af --user-data file://ec2_userdata.txt
 
