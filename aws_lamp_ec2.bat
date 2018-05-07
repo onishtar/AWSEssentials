498b54e0c5a3f54086b854c38a0530e445c8594e
onishtar <onishtar@hotmail.com>
2018-05-07 09:11:02 -0400

    added parm to script

--- aws_lamp_ec2.bat
+++ aws_lamp_ec2.bat
@@ -1,3 +1,2 @@
-#!/bin/bash
 aws ec2 run-instances --image-id ami-467ca739 --count 1 --instance-type t2.micro --key-name Essentials_Key --security-group-ids sg-c010f988 --subnet-id subnet-4906b266 --user-data file://aws_lamp_userdata.txt
 
